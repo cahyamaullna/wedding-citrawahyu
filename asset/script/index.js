5b577fbe8b41195287fbc450b9c8d96b47e52de4
cahya.maulana <cahya.maulana@iconpln.co.id>
2022-10-20 14:03:04 +0700

    2nd commit

--- asset/script/index.js
+++ asset/script/index.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("nov 5, 2022 12:00:00").getTime();
+var countDownDate = new Date("nov 5, 2022 10:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
